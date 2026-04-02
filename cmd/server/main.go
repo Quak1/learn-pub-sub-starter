a416ea618be724133e22c126471845263c397fb1
Aldo Camacho <me@aldocamacho.com>
2026-04-02 11:23:42 -0700

    send message to exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,23 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connection to RabbitMQ was successful")
+
+	pubCh, err := conn.Channel()
+	if err != nil {
+		log.Fatal("could not create channel: ", err)
+	}
+
+	err = pubsub.PublishJSON(
+		pubCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		})
+	if err != nil {
+		fmt.Println("could not publish data: ", err)
+	}
+	fmt.Println("Message sent")
 
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt)
