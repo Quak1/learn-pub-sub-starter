b26d1b3f8f4ebd2c6131805045d37235f101b6c8
Aldo Camacho <me@aldocamacho.com>
2026-04-02 11:04:08 -0700

    connect to local RabbitMQ server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const connString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatal("couldn't connect to RabbitMQ server: ", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection to RabbitMQ was successful")
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+
+	fmt.Println("Server is shutting down")
 }
